bbf48709bcb3e1fb17cc10ae3ea9a1257fa6100e
Leela Venkata Vundavalli <lvramaprasad@hotmail.com>
2020-02-02 01:35:11 -0500

    Initial Version.

--- lvComponent/v1/pdffunctions.js
+++ lvComponent/v1/pdffunctions.js
@@ -1,3 +1,29 @@
+function loadPage(url, pageNumber, scale) {
+    var pdfjsLib = window['pdfjs-dist/build/pdf'];
+
+    pdfjsLib.GlobalWorkerOptions.workerSrc = './assets/pdf.worker.js';
+    var loadingTask = pdfjsLib.getDocument(url);
+    loadingTask.promise.then(function(pdf) {
+      pdf.getPage(pageNumber).then(function(page) {
+        var viewport = page.getViewport({scale: scale});
+        var canvas = document.getElementById('the-canvas');
+        var context = canvas.getContext('2d');
+        canvas.height = viewport.height;
+        canvas.width = viewport.width;
+
+        var renderContext = {
+          canvasContext: context,
+          viewport: viewport
+        };
+        var renderTask = page.render(renderContext);
+        renderTask.promise.then(function () {
+        });
+      });
+    }, function (reason) {
+      console.error(reason);
+    });
+}
+
 function highlight(xPos, yPos, xSize, ySize, color) {
     var pdf = document.getElementById("the-canvas")
     if (pdf) {
@@ -21,6 +47,7 @@ function highlight(xPos, yPos, xSize, ySize, color) {
         if (page) {
             pagecanvas.beginPath();
             pagecanvas.rect(xPos, yPos, xSize, ySize);
+            pagecanvas.globalAlpha = 0.4;
             let highlightcolor = (!color ? 'rgba(66,216,66,0.5)' : color);
             pagecanvas.fillStyle = highlightcolor;
             pagecanvas.fill();
